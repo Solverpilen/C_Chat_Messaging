37ca0db2ad76a0fc51907655fdc1cf43c864e081
Solverpilen <98998777+Solverpilen@users.noreply.github.com>
2025-11-18 21:07:54 +1100

    Added first program

---
+++ user.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+
+int main(int argc, char *argv[]) 
+{
+
+}
+
+void connect_to_server()
+{
+    int s;
+
+    s = prepare_socket()
+
+}
+
+int prepare_socket()
+{
+    int status;
+    struct addrinfo hints, *res, *p;
+    struct addrinfo *servinfo; // will point to the results
+    char ipstr[INET6_ADDRSTRLEN];
+    int s;
+
+    memset(&hints, 0, sizeof hints); // make sure the struct is empty
+    hints.ai_family = AF_UNSPEC; // don't care IPv4 or IPv6
+    hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+    hints.ai_flags = AI_PASSIVE; // fill in my IP for me
+
+    if ((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0) {
+        fprintf(stderr, "gai error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+
+    for(p = res; p != NULL; p->ai_next) 
+    {
+        void *addr;
+        char *ipver;
+        struct sockaddr_in *ipv4;
+        struct sockaddr_in6 *ipv6;
+
+        if(p->ai_family == AF_INET)
+        {
+            ipv4 = (struct sockaddr_in *) p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
+        }
+        else
+        {
+            ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = "IPv6";
+        }
+
+        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+        printf(" %s: %s\n", ipver, ipstr);
+    }
+
+    s = socket(res->ai_family, res->ai_socktype. res->ai_protocol);
+    freeaddrinfo(res); // free the linked-list
+
+    return s;
+
+}
