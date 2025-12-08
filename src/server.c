815b002001808829fe3149e9a2ee6c78dde21719
Solverpilen <j.chumachenko@outlook.com>
2025-12-08 20:19:55 +1100

    Added init and select machine function to server.c

--- src/server.c
+++ src/server.c
@@ -7,20 +7,45 @@
 #include <netinet/in.h>
 #include "connection.h"
 #include "machine.h"
+#include <stdlib.h>
 
+typedef struct users {
 
-struct void * server_select_machine(char *user_name)
+    machine *user;
+    machine *next;
+
+} users;
+
+void * server_select_machine(machine * self, char *user_name)
 {
+    if(self->users)
+    {
+        users *user_list = (users *)self->users;
+
+        for(user_list; user_list->next != NULL; user_list->next)
+        {
+            if (strcmp(user_name, user_list->user->user_name) == 0)
+            {
+                return user_name;
+            }
+        }
+    } 
     
+    return NULL;
 }
 
-struct machine *machine_init(
-
-    machine *self, 
-    char *user_name, 
-    struct addrinfo *address_info,
-    struct connection *connection
-)
+struct machine * machine_init(char * user_name)
 {
-    
+    machine *s = malloc(sizeof(machine));
+    if(!s) return NULL;
+
+    s->user_name = strdup(user_name);
+    s->address_info = NULL;
+    s->connection = NULL;
+    s->users = NULL;
+
+    s->select_machine = server_select_machine;
+
+    return s;
+
 }
